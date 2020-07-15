605b33fc2c8ace029088ab7ab54a5f5a7ceac3da
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2020-07-15 21:13:20 +0200

    tests(*): add test coverage

--- src/app/+cities/components/cities-table/__tests__/cities-table.component.spec.ts
+++ src/app/+cities/components/cities-table/__tests__/cities-table.component.spec.ts
@@ -1,15 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatTableModule,
-  MatSortModule,
-  MatPaginator,
   MatInput,
   MatFormField,
+  MatPaginatorModule,
+  MatSortModule,
 } from '@angular/material';
+import { MockComponent } from 'ng-mocks';
 
 import { CitiesTableComponent } from '../cities-table.component';
 import { citiesResponseMock } from '../../../../mocks';
-import { MockComponent, MockModule } from 'ng-mocks';
 
 describe('CitiesTableComponent', () => {
   let component: CitiesTableComponent;
@@ -17,10 +18,14 @@ describe('CitiesTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatTableModule, MockModule(MatSortModule)],
+      imports: [
+        BrowserAnimationsModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+      ],
       declarations: [
         CitiesTableComponent,
-        MockComponent(MatPaginator),
         MockComponent(MatFormField),
         MockComponent(MatInput),
       ],
@@ -36,5 +41,10 @@ describe('CitiesTableComponent', () => {
 
   it('should match snapshot', () => {
     expect(fixture).toMatchSnapshot();
+  });
+
+  it('should update filter on input', () => {
+    component.applyFilter(' asdF');
+    expect(component.dataSource.filter).toBe('asdf');
   });
 });
